36ee07bd8d8afe5aefece597764edbe2601bf25d
Hoai Phong <42806464+Hoaiphong0509@users.noreply.github.com>
2021-06-10 11:29:14 +0700

    End of section 7

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -22,10 +22,11 @@ export class RegisterComponent implements OnInit {
     this.accountService.register(this.model).subscribe(response=>{
       console.log(response);
       this.cancel()
-    }, error=> {
-      console.log(error);
-      this.toastr.error(error.error)
     })
+    // , error=> {
+    //   console.log(error);
+    //   this.toastr.error(error.error)
+    // })
   }
 
   cancel(){
