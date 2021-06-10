3dae37583e6ff93bbeb3a08056a3f05746d9bd06
Hoai Phong <42806464+Hoaiphong0509@users.noreply.github.com>
2021-06-10 14:43:23 +0700

    End of section 8

--- API/Data/Migrations/20210610071812_FixBirthDateColumn.Designer.cs
+++ API/Data/Migrations/20210610071812_FixBirthDateColumn.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace API.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20210610071812_FixBirthDateColumn")]
+    partial class FixBirthDateColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
