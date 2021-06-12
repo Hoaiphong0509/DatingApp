7ef1c55bea846f1c4969ac04f7b79471dd0cb183
Hoai Phong <42806464+Hoaiphong0509@users.noreply.github.com>
2021-06-12 07:29:11 +0700

    End of section 10

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,3 +1,4 @@
+import { Observable } from 'rxjs';
 import { Member } from './../../_models/member';
 import { MembersService } from './../../_services/members.service';
 import { Component, OnInit } from '@angular/core';
@@ -8,20 +9,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-  members!: Member[];
+  members$!: Observable<Member[]>;
 
   constructor(private memberService: MembersService) { }
 
   ngOnInit(){
-    this.loadMembers();
+    this.members$ = this.memberService.getMembers();
   }
-
-  loadMembers(){
-    this.memberService.getMembers().subscribe(members => {
-      this.members = members
-    }, error => {
-      console.log(error)
-    })
-  }
-
 }
