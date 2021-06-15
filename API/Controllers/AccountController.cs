7867667da9e684254e61813101c07083f9c4a2c3
Hoai Phong <42806464+Hoaiphong0509@users.noreply.github.com>
2021-06-15 13:19:03 +0700

    connectionstring fix

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -16,7 +16,7 @@ namespace API.Controllers
         private readonly ITokenService _tokenService;
         private readonly IMapper _mapper;
         private readonly SignInManager<AppUser> _signInManager;
-        public AccountController(UserManager<AppUser> userManager,SignInManager<AppUser> signInManager,
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager,
             ITokenService tokenService, IMapper mapper)
         {
             _signInManager = signInManager;
@@ -25,61 +25,60 @@ namespace API.Controllers
             _tokenService = tokenService;
         }
 
-    [HttpPost("register")]
-    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
-    {
-        if (await UserExists(registerDto.Username))
-            return BadRequest("Username is taken");
-
-        var user = _mapper.Map<AppUser>(registerDto);
-
-        user.UserName = registerDto.Username.ToLower();
-
-        var result = await _userManager.CreateAsync(user, registerDto.Password);
-
-        if(!result.Succeeded) return BadRequest(result.Errors);
-
-        var roleResult = await _userManager.AddToRoleAsync(user, "Member");
-
-        if(!roleResult.Succeeded) return BadRequest(result.Errors);
-
-        return new UserDto
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
-            Username = user.UserName,
-            Token = await _tokenService.CreateToken(user),
-            KnownAs = user.KnownAs,
-            Gender = user.Gender
-        };
-    }
+            if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
 
-    [HttpPost("login")]
-    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
-    {
-        var user = await _userManager.Users
-            .Include(p => p.Photos)
-            .SingleOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
+            var user = _mapper.Map<AppUser>(registerDto);
 
-        if (user is null) return Unauthorized("Invalid username!");
+            user.UserName = registerDto.Username.ToLower();
 
-        var result = await _signInManager
-            .CheckPasswordSignInAsync(user, loginDto.Password, false);
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
 
-        if(!result.Succeeded) return Unauthorized();    
+            if (!result.Succeeded) return BadRequest(result.Errors);
 
-        return new UserDto
+            var roleResult = await _userManager.AddToRoleAsync(user, "Member");
+
+            if (!roleResult.Succeeded) return BadRequest(result.Errors);
+
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = await _tokenService.CreateToken(user),
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
+            };
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            Username = user.UserName,
-            Token = await _tokenService.CreateToken(user),
-            PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
-            KnownAs = user.KnownAs,
-            Gender = user.Gender
-        };
+            var user = await _userManager.Users
+                .Include(p => p.Photos)
+                .SingleOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
 
-    }
+            if (user is null) return Unauthorized("Invalid username!");
 
-    private async Task<bool> UserExists(string username)
-    {
-        return await _userManager.Users.AnyAsync(x => x.UserName == username.ToLower());
+            var result = await _signInManager
+                .CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (!result.Succeeded) return Unauthorized();
+
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = await _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
+            };
+
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _userManager.Users.AnyAsync(x => x.UserName == username.ToLower());
+        }
     }
-}
 }
--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -16,46 +16,44 @@ export class AccountService {
 
   constructor(private http: HttpClient, private presence: PresenceService) { }
 
-  login(model: any){
-    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+  login(model: any) {
+    return this.http.post(this.baseUrl + 'account/login', model).pipe(
       map((response: User) => {
         const user = response;
-        if(user){
+        if (user) {
           this.setCurrentUser(user);
           this.presence.createHubConnection(user);
         }
       })
-    );
+    )
   }
 
-  register(model: any){
-    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
-      map((user: User)=>{
-        if(user){
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'account/register', model).pipe(
+      map((user: User) => {
+        if (user) {
           this.setCurrentUser(user);
           this.presence.createHubConnection(user);
         }
       })
-    );
+    )
   }
 
-  setCurrentUser(user: User){
+  setCurrentUser(user: User) {
     user.roles = [];
     const roles = this.getDecodedToken(user.token).role;
-
     Array.isArray(roles) ? user.roles = roles : user.roles.push(roles);
-
-    localStorage.setItem('user', JSON.stringify(user))
+    localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
   }
 
-  logout(){
+  logout() {
     localStorage.removeItem('user');
-    this.currentUserSource.next();
+    this.currentUserSource.next(null);
     this.presence.stopHubConnection();
   }
-
-  getDecodedToken(token){
+  
+  getDecodedToken(token) {
     return JSON.parse(atob(token.split('.')[1]));
   }
 }
